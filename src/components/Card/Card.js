a95b8081cb718337bb826b111e35e523e070c0d6
Sherzod-Tohirov <sherzodtohirov862@gmail.com>
2023-10-31 15:06:13 +0500

    Make few changes

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,24 +1,25 @@
 function Card({data}) {
     let key = 0;
     return (
-        <li className="pokemons__item w-80 backdrop-blur-sm bg-white/30 bg-sky-500 text-slate-100 p-4 rounded-xl">
+        <li className="pokemons__item flex flex-col items-center w-80 backdrop-blur-sm bg-white/30 bg-sky-500 text-slate-100 p-4 rounded-xl">
             <h3 className="text-center text-3xl mb-4">{data.name}</h3>
             <img className="w-full mb-3" src={data.img} alt={data.name} />
-            <div className="flex flex-wrap gap-4">
-                <p className="text-lg"><strong>Weight:</strong> {data.weight}</p>
-                <p className="text-lg"><strong>Height:</strong> {data.height}</p>
-                <p className="text-lg"><strong>Candy Count: </strong> {data.candy_count || 0}</p>
-                <p className="text-lg"><strong>Egg:</strong> {data.egg}</p>
-                <ul className="w-full flex flex-col flex-wrap text-lg font-bold gap-2">
-                    Weaknesses: 
-                    {   
-                        data.weaknesses.map(item => {
-                            return <li key={++key} className="font-semibold">• {item}</li>
-                        })
-                    }
-                </ul>
-                
-            </div>
+            <ul className="flex flex-grow flex-wrap gap-4">
+                <li className="text-lg bg-teal-300 backdrop-blur-sm p-3 rounded-lg"><strong>📏 Weight:</strong> {data.weight}</li>
+                <li className="text-lg bg-teal-300 backdrop-blur-sm p-3 rounded-lg"><strong>📐 Height:</strong> {data.height}</li>
+                <li className="text-lg bg-teal-300 backdrop-blur-sm p-3 rounded-lg"><strong>🍬 Candy Count: </strong> {data.candy_count || 0}</li>
+                <li className="text-lg bg-teal-300 backdrop-blur-sm p-3 rounded-lg"><strong>🪺 Egg:</strong> {data.egg}</li>
+                <li className="w-full flex-grow bg-teal-400 backdrop-blur-sm p-3 rounded-lg">
+                    <ul className="w-full h-full flex flex-col flex-wrap text-lg font-bold gap-2">
+                        Weaknesses: 
+                        {   
+                            data.weaknesses.map(item => {
+                                return <li key={++key} className="font-semibold">❌ {item}</li>
+                            })
+                        }
+                    </ul>
+                </li>
+            </ul>   
         </li>
     );
 }
